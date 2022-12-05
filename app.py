1916c1b5c889968d305e335398c819093151acce
Man Mai <manmai.huu@pdax.ph>
2022-12-06 00:47:59 +0700

    Remove update routes

--- app.py
+++ app.py
@@ -4,13 +4,11 @@ from fastapi_pagination import add_pagination
 from auth.jwt_bearer import JWTBearer
 from config.config import initiate_database
 from routes.admin import router as AdminRouter
-from routes.student import router as StudentRouter
 from routes.super_admin import router as SuperAdminRouter
 from routes.user import router as UserRouter
 from routes.exercises import router as ExercisesRouter
 from routes.logs import router as LogsRouter
 from routes.diet import router as DietRouter
-from routes.upload import router as UploadRouter
 
 app = FastAPI()
 
@@ -29,10 +27,8 @@ async def read_root():
 
 
 app.include_router(AdminRouter, tags=["Administrator"], prefix="/admin")
-app.include_router(StudentRouter, tags=["Students"], prefix="/student", dependencies=[Depends(token_listener)])
 app.include_router(SuperAdminRouter, tags=["Super Admin"], prefix="/super-admin",)
 app.include_router(UserRouter, tags=["Users"], prefix="/user", )
 app.include_router(ExercisesRouter, tags=["Exercises"], prefix="/exercises", )
 app.include_router(DietRouter, tags=["Diet"], prefix="/diet", )
 app.include_router(LogsRouter, tags=["Logs"], prefix="/logs", )
-app.include_router(UploadRouter, tags=["Upload"], prefix="/upload", )
