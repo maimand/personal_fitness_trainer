862b81f6dcc1d4fa0e8162d2282c3d086de3c2d9
Man Mai <42661871+maimand@users.noreply.github.com>
2022-12-25 16:31:51 +0700

    Update mail format

--- services/mail.py
+++ services/mail.py
@@ -1,5 +1,6 @@
 import smtplib
 import ssl
+from email.message import EmailMessage
 
 port = 587  # For starttls
 smtp_server = "smtp.gmail.com"
@@ -8,42 +9,55 @@ password = 'evohymayavnxcuky'
 
 
 def send_email(email: str, code: str):
-    message = """\
-    Subject: Hi there
+    msg = EmailMessage()
+    msg.set_content('You code to register is: {}.'.format(code))
 
-    You code to register is: {}.""".format(code)
+    msg['Subject'] = 'Activation code'
+    msg['From'] = 'personal.fitness000@gmail.com'
+    msg['To'] = email
     context = ssl.create_default_context()
     with smtplib.SMTP(smtp_server, port) as server:
         server.ehlo()  # Can be omitted
         server.starttls(context=context)
         server.ehlo()  # Can be omitted
         server.login(sender_email, password)
-        server.sendmail(sender_email, email, message)
+        server.send_message(msg)
+        server.quit()
 
 
 def send_disable_email(email: str):
-    message = """\
-    Subject: Hi there
+    msg = EmailMessage()
+    msg.set_content('You account has been enabled. Contact your admin for more information')
 
-    You account has been disabled by admin. Please contact our website to reach for help."""
+    msg['Subject'] = 'Account disable'
+    msg['From'] = 'personal.fitness000@gmail.com'
+    msg['To'] = email
+
     context = ssl.create_default_context()
     with smtplib.SMTP(smtp_server, port) as server:
         server.ehlo()  # Can be omitted
         server.starttls(context=context)
         server.ehlo()  # Can be omitted
         server.login(sender_email, password)
-        server.sendmail(sender_email, email, message)
+        server.send_message(msg)
+        server.quit()
 
 
 def send_enable_email(email: str):
-    message = """\
-    Subject: Hi there
+    msg = EmailMessage()
+    msg.set_content('You account has been enabled. You now can login with email and password registered')
 
-    You account has been enabled. You now can login with email and password registered."""
+    msg['Subject'] = 'Account enabled'
+    msg['From'] = 'personal.fitness000@gmail.com'
+    msg['To'] = email
+
     context = ssl.create_default_context()
     with smtplib.SMTP(smtp_server, port) as server:
         server.ehlo()  # Can be omitted
         server.starttls(context=context)
         server.ehlo()  # Can be omitted
         server.login(sender_email, password)
-        server.sendmail(sender_email, email, message)
+        server.send_message(msg)
+        server.quit()
+
+send_email('manmaihuu@gmail.com', '123456')
