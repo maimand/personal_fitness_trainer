ba1acff72f9513f91f86dea2f86b97a66da85d4e
Man Mai <42661871+maimand@users.noreply.github.com>
2022-12-26 23:54:07 +0700

    Fix bugs

--- services/diet.py
+++ services/diet.py
@@ -3,22 +3,40 @@ import pandas as pd
 from models.diet import Food
 from difflib import SequenceMatcher
 
+df = pd.read_csv('services/normalized_data.csv')
+df = df.fillna(0)
+
 
 def get_food_data(food) -> Food:
-    df = pd.read_csv('services/normalized_data.csv')
-    selected_row = df.loc[SequenceMatcher(None, df['Normalized'].str, food).ratio() > 0.5]
-    if not selected_row.empty:
-        return Food(
-            id=food,
-            name=selected_row.values[0][0],
-            ration=selected_row.values[0][1],
-            calo=selected_row.values[0][2],
-            protein=selected_row.values[0][3],
-            fat=selected_row.values[0][4],
-            carb=selected_row.values[0][5],
-            fiber=selected_row.values[0][6],
-            image=selected_row.values[0][8],
-        )
+    for ind in df.index:
+        if SequenceMatcher(None, df['Normalized'][ind], food).ratio() > 0.8:
+            selected_row = df.iloc[[ind]]
+            res = map_list_to_food(selected_row.values[0])
+            # res = Food(
+            #     id=food,
+            #     name=selected_row.values[0][0],
+            #     ration=selected_row.values[0][1],
+            #     calo=selected_row.values[0][2],
+            #     protein=selected_row.values[0][3],
+            #     fat=selected_row.values[0][4],
+            #     carb=selected_row.values[0][5],
+            #     fiber=selected_row.values[0][6],
+            #     image=selected_row.values[0][8],
+            # )
+            return res
+    # if not selected_row:
+    #     res = Food(
+    #         id=food,
+    #         name=selected_row[0].values[0][0],
+    #         ration=selected_row[0].values[0][1],
+    #         calo=selected_row[0].values[0][2],
+    #         protein=selected_row[0].values[0][3],
+    #         fat=selected_row[0].values[0][4],
+    #         carb=selected_row[0].values[0][5],
+    #         fiber=selected_row[0].values[0][6],
+    #         image=selected_row[0].values[0][8],
+    #     )
+    #     return res
 
 
 def map_list_to_food(input_list) -> Food:
@@ -35,11 +53,17 @@ def map_list_to_food(input_list) -> Food:
 
 
 def get_all_food_data(food) -> List[Food]:
-    df = pd.read_csv('services/normalized_data.csv')
+    foods = []
     if food:
-        foods = df.loc[df['Normalized'].str.contains(food)].values.tolist()
+        for ind in df.index:
+            if SequenceMatcher(None, df['Normalized'][ind], food).ratio() > 0.4:
+                selected_row = df.iloc[[ind]]
+                foods.append(selected_row.values[0])
     else:
         foods = df.values.tolist()
     food_list = list(map(lambda a: map_list_to_food(a), foods))
     if food_list:
         return food_list
+    else:
+        return []
+
